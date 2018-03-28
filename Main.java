bad7043bc8f67f4786e18867fe25b61a14e0a56d
testForProject13 <37381162+testForProject13@users.noreply.github.com>
2018-03-28 19:38:46 +0100

    test

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main{
 	public static void main(String[] args) {
 		//This prints Hello World! to the console
 		System.out.println("Hello World!");
-      	System.out.println("Hello GitHub");
+      	System.out.println("Hello ");
 	}
 }
